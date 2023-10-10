d8c70b247ccf4521cabac623b042e2275b798385
Arun Natarajan <arunnatarajan@aruns-mbp.lan>
2023-10-09 23:58:42 -0500

    Edited CTA

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -5,7 +5,7 @@ import {BsLinkedin} from 'react-icons/bs'
 const CTA = () => {
   return (
 	<div className='cta'>
-		<a href="https://www.linkedin.com/in/arun-natarajan-667674200/" target="_blank" className='header_socials'><BsLinkedin/></a>
+		<a href="https://www.linkedin.com/in/arun-natarajan-667674200/" target="_blank" rel="noreferrer" className='header_socials'><BsLinkedin/></a>
 		<a href={Resume} download className='btn'>Download Resume</a>
 		<a href="#contact" className='btn btn-primary'>Let's Connect</a>
 	</div>
