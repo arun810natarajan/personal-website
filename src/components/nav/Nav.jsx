4def2996af6ebb9450bf9444f7d35bdaa8e3e70a
Arun Natarajan <arunnatarajan@aruns-mbp.lan>
2023-10-10 00:16:13 -0500

    Fixed errors

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -10,8 +10,8 @@ import {useState} from 'react'
 const Nav = () => {
   const[activeNav, setActiveNav] = useState('#')
   return (
-	<nav>
-    <a href="#" onClick={() => setActiveNav('#')} className={activeNav ==='#' ? 'active' : ''}><AiOutlineHome/></a>
+  <nav>
+    <a href="/" onClick={() => setActiveNav('#')} className={activeNav ==='#' ? 'active' : ''}><AiOutlineHome/></a>
     <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav ==='#about' ? 'active' : ''} ><AiOutlineUserSwitch/></a>
     <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav ==='#experience' ? 'active' : ''}><LuGraduationCap/></a>
     <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav ==='#contact' ? 'active' : ''}><AiOutlinePhone/></a>
